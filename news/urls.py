5a6ffb6e34c304de8a49deaf9f781cd66ea6217f
Dmitriy Legeckiy <90146637+DimHam163@users.noreply.github.com>
2023-04-14 19:02:16 +0400

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create/', NewsCreate.as_view(), name='post_create'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_update'),
     path('<int:pk>/delete/', NewsDelete.as_view(), name='post_delete'),
-    path('articles/create/', NewsCreate.as_view(), name='articles_edit'),
+    path('articles/create/', NewsCreate.as_view(), name='article_edit'),
     path('articles/<int:pk>/edit/', NewsUpdate.as_view(), name='article_update'),
     path('articles/<int:pk>/delete/', NewsDelete.as_view(), name='article_delete'),
 ]
